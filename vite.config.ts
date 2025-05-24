f37b5508a72240160a132fc7e86dbabddca0f97b
Lukorama <58047304+Lukorama@users.noreply.github.com>
2025-05-24 14:42:49 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,13 +5,12 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "./", // 👈 ADICIONE ESTA LINHA AQUI
   server: {
     host: "::",
     port: 3005,
   },
-  plugins: [react(), mode === "development" && componentTagger()].filter(
-    Boolean
-  ),
+  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
